f10e5eb1f89b4f721aca6900ba520d0cc1aad235
David-Tonsing-Dev <davidtonsing1123@gmail.com>
2024-07-08 15:42:30 +0530

    added coin predication

--- lib/screens/add_coin.dart
+++ lib/screens/add_coin.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:math'; // Import the math library
 
 class AddCoinScreen extends StatefulWidget {
   const AddCoinScreen({super.key});
@@ -11,11 +12,13 @@ class AddCoinScreen extends StatefulWidget {
 
 class _AddCoinScreenState extends State<AddCoinScreen> {
   final TextEditingController _searchController = TextEditingController();
+  final TextEditingController _investmentController = TextEditingController();
   List<dynamic> _searchResults = [];
   bool _isLoading = false;
   bool _showSearchResults = true;
   dynamic _selectedCoin;
   Map<String, dynamic>? _coinDetails;
+  String _selectedDuration = '1 year';
 
   Future<void> _searchCoins(String query) async {
     if (query.isEmpty) {
@@ -66,9 +69,16 @@ class _AddCoinScreenState extends State<AddCoinScreen> {
     }
   }
 
+  double _calculatePrediction(double amount, String duration) {
+    double annualGrowthRate = 0.1; // Example annual growth rate of 10%
+    int years = int.parse(duration.split(' ')[0]);
+    return amount * pow((1 + annualGrowthRate), years);
+  }
+
   @override
   void dispose() {
     _searchController.dispose();
+    _investmentController.dispose();
     super.dispose();
   }
 
@@ -157,59 +167,162 @@ class _AddCoinScreenState extends State<AddCoinScreen> {
                       )
                     : Container(),
             _selectedCoin != null && _coinDetails != null
-                ? Card(
-                    color: Color(0xFF222224),
-                    margin: const EdgeInsets.only(top: 16.0),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              Image.network(
-                                _selectedCoin['thumb'],
-                                height: 50,
-                                width: 50,
-                              ),
-                              const SizedBox(width: 16.0),
-                              Text(
-                                _selectedCoin['name'],
-                                style: const TextStyle(
-                                  fontSize: 24.0,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
+                ? Expanded(
+                    child: ListView(
+                      children: [
+                        Card(
+                          color: Color(0xFF222224),
+                          margin: const EdgeInsets.only(top: 16.0),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Image.network(
+                                      _selectedCoin['thumb'],
+                                      height: 50,
+                                      width: 50,
+                                    ),
+                                    const SizedBox(width: 16.0),
+                                    Text(
+                                      _selectedCoin['name'],
+                                      style: const TextStyle(
+                                        fontSize: 24.0,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ),
-                            ],
+                                const SizedBox(height: 16.0),
+                                Text(
+                                  'Symbol: ${_selectedCoin['symbol']}',
+                                  style: const TextStyle(
+                                      fontSize: 18.0, color: Colors.white),
+                                ),
+                                const SizedBox(height: 8.0),
+                                Text(
+                                  'Market Cap Rank: ${_selectedCoin['market_cap_rank'] ?? 'N/A'}',
+                                  style: const TextStyle(
+                                      fontSize: 18.0, color: Colors.white),
+                                ),
+                                const SizedBox(height: 8.0),
+                                Text(
+                                  'Current Price: \$${_coinDetails?['market_data']['current_price']['usd'] ?? 'N/A'}',
+                                  style: const TextStyle(
+                                      fontSize: 18.0, color: Colors.white),
+                                ),
+                                const SizedBox(height: 8.0),
+                                Text(
+                                  'Total Market Cap: \$${_coinDetails?['market_data']['market_cap']['usd'] ?? 'N/A'}',
+                                  style: const TextStyle(
+                                      fontSize: 18.0, color: Colors.white),
+                                ),
+                                const SizedBox(height: 16.0),
+                                TextField(
+                                  controller: _investmentController,
+                                  decoration: InputDecoration(
+                                    labelText: 'Amount to invest',
+                                    labelStyle: TextStyle(
+                                        color:
+                                            Color.fromRGBO(55, 204, 155, 1.0)),
+                                    border: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Color.fromRGBO(
+                                              55, 204, 155, 1.0)),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Color.fromRGBO(
+                                              55, 204, 155, 1.0)),
+                                    ),
+                                    enabledBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Color.fromRGBO(
+                                              55, 204, 155, 1.0)),
+                                    ),
+                                    hintText: 'Enter amount',
+                                    hintStyle: TextStyle(
+                                        color:
+                                            Color.fromRGBO(55, 204, 155, 1.0)),
+                                  ),
+                                  style: TextStyle(color: Colors.white),
+                                  keyboardType: TextInputType.number,
+                                ),
+                                const SizedBox(height: 16.0),
+                                DropdownButtonFormField<String>(
+                                  value: _selectedDuration,
+                                  items: ['1 year', '2 years', '5 years']
+                                      .map((String value) {
+                                    return DropdownMenuItem<String>(
+                                      value: value,
+                                      child: Text(value),
+                                    );
+                                  }).toList(),
+                                  onChanged: (newValue) {
+                                    setState(() {
+                                      _selectedDuration = newValue!;
+                                    });
+                                  },
+                                  decoration: InputDecoration(
+                                    labelText: 'Select duration',
+                                    labelStyle: TextStyle(
+                                        color:
+                                            Color.fromRGBO(55, 204, 155, 1.0)),
+                                    border: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Color.fromRGBO(
+                                              55, 204, 155, 1.0)),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Color.fromRGBO(
+                                              55, 204, 155, 1.0)),
+                                    ),
+                                    enabledBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Color.fromRGBO(
+                                              55, 204, 155, 1.0)),
+                                    ),
+                                  ),
+                                  dropdownColor: Color(0xFF222224),
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                const SizedBox(height: 16.0),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    double amount = double.parse(
+                                        _investmentController.text);
+                                    double prediction = _calculatePrediction(
+                                        amount, _selectedDuration);
+                                    showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return AlertDialog(
+                                          title: Text('Investment Prediction'),
+                                          content: Text(
+                                              'Your investment of \$${amount.toStringAsFixed(2)} could grow to \$${prediction.toStringAsFixed(2)} in $_selectedDuration.'),
+                                          actions: <Widget>[
+                                            TextButton(
+                                              child: Text('Close'),
+                                              onPressed: () {
+                                                Navigator.of(context).pop();
+                                              },
+                                            ),
+                                          ],
+                                        );
+                                      },
+                                    );
+                                  },
+                                  child: Text('Show Prediction'),
+                                ),
+                              ],
+                            ),
                           ),
-                          const SizedBox(height: 16.0),
-                          Text(
-                            'Symbol: ${_selectedCoin['symbol']}',
-                            style: const TextStyle(
-                                fontSize: 18.0, color: Colors.white),
-                          ),
-                          const SizedBox(height: 8.0),
-                          Text(
-                            'Market Cap Rank: ${_selectedCoin['market_cap_rank'] ?? 'N/A'}',
-                            style: const TextStyle(
-                                fontSize: 18.0, color: Colors.white),
-                          ),
-                          const SizedBox(height: 8.0),
-                          Text(
-                            'Current Price: \$${_coinDetails?['market_data']['current_price']['usd'] ?? 'N/A'}',
-                            style: const TextStyle(
-                                fontSize: 18.0, color: Colors.white),
-                          ),
-                          const SizedBox(height: 8.0),
-                          Text(
-                            'Total Market Cap: \$${_coinDetails?['market_data']['market_cap']['usd'] ?? 'N/A'}',
-                            style: const TextStyle(
-                                fontSize: 18.0, color: Colors.white),
-                          ),
-                          // Add more details as needed
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   )
                 : Container(),
