b13504478113242c500abed0b45d59a747da9a91
abdGhost <ktaher642@gmail.com>
2024-08-13 15:35:52 +0530

    added

--- lib/screens/game_redem.dart
+++ lib/screens/game_redem.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class GameRedem extends StatefulWidget {
   const GameRedem({super.key});
@@ -69,8 +70,8 @@ class _GameRedemState extends State<GameRedem> {
                   ],
                 ),
                 Container(
-                  width: 60,
-                  height: 60,
+                  width: 80,
+                  height: 80,
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius:
@@ -80,7 +81,45 @@ class _GameRedemState extends State<GameRedem> {
                       width: 1, // Width of the border
                     ),
                   ),
-                ),
+                  child: Stack(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10, top: 5),
+                        child: FaIcon(
+                          FontAwesomeIcons.key,
+                          size: 50,
+                          color: Color(0xFF348f6c),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 4,
+                        right: 4,
+                        child: Container(
+                          width: 30,
+                          height: 30,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(
+                                4), // Makes the container rounded
+                            border: Border.all(
+                              color: Colors.black12, // Color of the border
+                              width: 1, // Width of the border
+                            ),
+                          ),
+                          child: Center(
+                            child: Text(
+                              '18',
+                              style: TextStyle(
+                                color: Colors.black54,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
             SizedBox(
@@ -104,8 +143,8 @@ class _GameRedemState extends State<GameRedem> {
               ),
               child: Column(
                 children: [
-                  Image.asset(
-                    "assets/rwa-mobile-login-icon.png",
+                  Image.network(
+                    "https://res.cloudinary.com/dbtsrjssc/image/upload/v1723464637/final_ion_1_l2eev9.png",
                     width: double.infinity,
                     height: 80,
                     fit: BoxFit.contain,
@@ -118,13 +157,112 @@ class _GameRedemState extends State<GameRedem> {
                       fontSize: 14,
                     ),
                   ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5, right: 5, top: 5),
+                    child: Row(
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.key,
+                          size: 18,
+                          color: Color(0xFF348f6c),
+                        ),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              '0/4',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                color: Colors.black54,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 2,
+                            ),
+                            Text(
+                              'received',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black54,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: 120,
+              height: 140,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius:
+                    BorderRadius.circular(10), // Makes the container rounded
+                border: Border.all(
+                  color: Colors.black12, // Color of the border
+                  width: 1, // Width of the border
+                ),
+              ),
+              child: Column(
+                children: [
+                  Image.network(
+                    "https://res.cloudinary.com/dbtsrjssc/image/upload/v1723464637/final_ion_1_l2eev9.png",
+                    width: double.infinity,
+                    height: 80,
+                    fit: BoxFit.contain,
+                  ),
                   Text(
-                    '',
+                    'RWA Quiz',
                     style: TextStyle(
                       fontWeight: FontWeight.w600,
                       color: Colors.black54,
+                      fontSize: 14,
                     ),
                   ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5, right: 5, top: 5),
+                    child: Row(
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.key,
+                          size: 18,
+                          color: Color(0xFF348f6c),
+                        ),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              '0/4',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                color: Colors.black54,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 2,
+                            ),
+                            Text(
+                              'received',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black54,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
